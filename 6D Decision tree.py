82eabe139b1fecdc5ad7249378c7ad614f11a57c
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2024-03-02 10:16:59 +0530

    Create 6D Decision tree.py

---
+++ 6D Decision tree.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv("C:/Users/kamini/Downloads/machine learning/IRIS.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 8)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the Decision Tree Classification model on the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 5)
+classifier.fit(X_train, y_train)
+
+# Display the Decision Tree
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+
+# Convert Index object to list
+feature_names = list(dataset.columns[:-1])
+plt.figure(figsize=(20,10))
+plot_tree(classifier, filled=True, rounded=True, feature_names=feature_names)
+plt.show()
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Display the results (confusion matrix and accuracy)
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
